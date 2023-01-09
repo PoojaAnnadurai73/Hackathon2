18a3a84a16a1a4b7af112768306f18dfac07f2da
PoojaAnnadurai73 <122273606+PoojaAnnadurai73@users.noreply.github.com>
2023-01-09 16:44:35 +0530

    Add files via upload

---
+++ MedicalResearchApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalResearchApp
+{
+    class DiseaseCaused
+    {
+        public int PathogenName { get; set; }
+        public string Severity { get; set; }
+        public string Symptoms { get; set; }
+        public string PatientName { get; set; }
+        public void ShallowCopy(DiseaseCaused copy)
+        {
+            this.PathogenName = copy.PathogenName;
+            this.Severity = copy.Severity;
+            this.Symptoms = copy.Symptoms;
+            this.PatientName = copy.PatientName;
+        }
+
+        public DiseaseCaused DeepCopy(DiseaseCaused copy)
+        {
+            DiseaseCaused disease = new DiseaseCaused();
+            disease.ShallowCopy(copy);
+            return disease;
+        }
+    }
+
+    enum ExternalFactors { MOs, POD, CFood, ImpNou }
+    class EnumExternalFactors
+    {
+       void EnumExternalFactors.Main(string[] args)
+        {
+            ExternalFactors EF = ExternalFactors.MOs;
+            Console.WriteLine("The External factor that is responsible for this disease is" + EF);
+            Console.WriteLine("The Other External Factors that are responsible for this disease is:");
+            Array ResponsibleEF = Enum.GetValues(typeof(ExternalFactors));
+            for (int e = 0; e < ResponsibleEF.Length; e++)
+                Console.WriteLine(ResponsibleEF.GetValue(e));
+            object inputvalue = Enum.Parse(typeof(ExternalFactors), Console.ReadLine(), true);
+            ExternalFactors AccurateEF = (ExternalFactors)inputvalue;
+            Console.WriteLine("The Disease Caused External Factor is" + AccurateEF);
+        }
+    }
+    enum InternalFactors { GFs, CMFs, ImpCelldiv }
+    class EnumInternalFactors
+    {
+        static void Main(string[] args)
+        {
+            {
+                InternalFactors IF = InternalFactors.GFs;
+                Console.WriteLine("The Internal Factors that is responsible for this disease is" + IF);
+                Console.WriteLine("The Other Internal Factors that are responsible for this disease is:");
+                Array ResponsibleIF = Enum.GetValues(typeof(InternalFactors));
+                for (int i = 0; i < ResponsibleIF.Length; i++)
+                    Console.WriteLine(ResponsibleIF.GetValue(i));
+                object inputvalue = Enum.Parse(typeof(InternalFactors), Console.ReadLine(), true);
+                InternalFactors AccurateIF = (InternalFactors)inputvalue;
+                Console.WriteLine("The Disease Caused External Factor is" + AccurateIF);
+            }
+        }
+    }
+}
+    
+        
+
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+namespace MedApp.Medical_App
+
+
+{
+    class PathogenInfo
+    {
+
+        public string DiseaseName { get; set; }
+        public string PatientName { get; set; }
+        public string PathogenName { get; set; }
+        public string Severity { get; set; }
+
+        public string Symptoms { get; set; }
+
+
+    }
+
+    class Symptoms : PathogenInfo
+    {
+        private ArrayList Disease = new ArrayList();
+        public bool AddDisease(PathogenInfo dss)
+        {
+            Disease.Add(dss);
+            Console.WriteLine("Added Successfully...");
+            return true;
+        }
+        public Array DisplayDis()
+        {
+
+            return Disease.ToArray();
+
+        }
+        public bool CheckSymptoms(string Sym)
+        {
+            for (int i = 0; i < Disease.Count; i++)
+            {
+                if (Disease[i] is PathogenInfo)
+                {
+                    var data = Disease[i] as PathogenInfo;
+                    if (data.Symptoms == Sym)
+                    {
+                        Console.WriteLine("The patient suffers/ may suffers from {0}", data.DiseaseName);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+    class PatientInfo : Symptoms
+    {
+        public string PatName { get; set; }
+        public string PatSym { get; set; }
+
+        public ArrayList PatDet { get; set; }
+
+        public void SetDet(PatientInfo pat)
+        {
+            PatDet.Add(pat);
+        }
+
+
+
+    }
+
+    class Patient
+    {
+        static public void Run()
+        {
+
+            Console.WriteLine(" 1 for Add Disease for Diagnosis");
+            Console.WriteLine(" 2 for Diagnosis");
+        Dis:
+            int choice = utilities.GetNumber("Options");
+            switch (choice)
+            {
+                case 1:
+                    HelperAdd();
+                    break;
+                default:
+                    Console.WriteLine("Oops!! Invalid Option Try again");
+                    goto Dis;
+            }
+
+        }
+
+        static public void HelperAdd()
+        {
+
+            Symptoms spm = new Symptoms();
+            void SeverityCheck(string SeverityBool)
+            {
+                if (SeverityBool.ToUpper() == "HIGHER" || SeverityBool.ToUpper() == "LOW" || SeverityBool.ToUpper() == "MEDIUM")
+                {
+                    Console.WriteLine("The Severity level checked");
+                }
+                else
+                {
+                    throw new Exception("Invalid");
+                }
+            }
+        P:
+            string DisName = utilities.Prompt("Pathogenic Name");
+
+            string Severity = utilities.Prompt(" Severity level of Disease \n\n HIGH\n MEDIUM \n LOW");
+            try
+            {
+                SeverityCheck(Severity);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                goto P;
+            }
+            string EnteredSymptoms = utilities.Prompt("Add Symptoms");
+
+
+            PathogenInfo dis = new PathogenInfo();
+            dis.DiseaseName = DisName;
+            dis.Severity = Severity;
+            dis.Symptoms = EnteredSymptoms;
+            spm.AddDisease(dis);
+
+
+
+            string ChoiceAdd = utilities.Prompt("Add Another disease");
+
+            if (ChoiceAdd.ToUpper() == "YES")
+            {
+                goto P;
+            }
+
+
+
+
+
+
+            string PatName = utilities.Prompt("Patient Name");
+            string PatSym = utilities.Prompt("Get To know about disease through symptoms");
+            
+            Console.WriteLine("");
+            Thread.Sleep(16800);
+
+            spm.CheckSymptoms(PatSpm);
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MedApp");
+            Console.WriteLine("To Diagnose the disease we advice you to add Disease Name");
+
+            Run();
+
+
+        }
+
+
+    }
+}
